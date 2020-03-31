23be8157e6d2dc95e8b15a1144cebd7922567e9c
mikearaya <mikaelaraya12@gmail.com>
2020-03-31 23:04:37 +0300

    removed unused files

--- stylelint.config.js
+++ stylelint.config.js
@@ -106,5 +106,10 @@ module.exports = {
     "value-list-comma-space-before": "never",
     "value-list-max-empty-lines": 0
   },
-  ignoreFiles: ["**/**/*.json", "./assets/images/**", "./assets/js/**"]
+  ignoreFiles: [
+    "**/**/*.json",
+    "./assets/images/**",
+    "./assets/js/**",
+    "./assets/favicons/**"
+  ]
 };
